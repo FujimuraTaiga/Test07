ee4d8225502bc3670ce9e21957067ad5cfef3c83
taiga-mac <fujimurataiga@icloud.com>
2022-05-24 20:51:33 +0900

    SpringBootの初期プロジェクトを置く

---
+++ src/test/java/com/example/Test07/Test07ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Test07;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Test07ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
