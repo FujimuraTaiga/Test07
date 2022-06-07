a0679fb11ba65f98b26c53b9f2604c536c20e5cc
taiga-mac <fujimurataiga@icloud.com>
2022-06-07 14:37:29 +0900

    データベースの初期化実装

---
+++ src/main/resources/schema.sql
@@ -0,0 +1,43 @@
+CREATE TABLE IF NOT EXISTS cafeteria_menu(
+    id VARCHAR(8),
+    name VARCHAR(32),
+    explain VARCHAR(4096),
+    image BLOB(65536),
+    PRIMARY KEY(id)
+);
+
+CREATE TABLE IF NOT EXISTS cafeteria_post(
+    postId VARCHAR(8),
+    menuId VARCHAR(8),
+    evaluation DOUBLE,
+    comment VARCHAR(4096),
+    PRIMARY KEY(postId)
+);
+
+CREATE TABLE IF NOT EXISTS class(
+    id VARCHAR(8),
+    name VARCHAR(32),
+    explain VARCHAR(4096),
+    PRIMARY KEY(id)
+);
+
+CREATE TABLE IF NOT EXISTS class_post(
+    postId VARCHAR(8),
+    classId VARCHAR(8),
+    evaluation DOUBLE,
+    comment VARCHAR(4096),
+    PRIMARY KEY(postId)
+);
+
+CREATE TABLE IF NOT EXISTS thread(
+    id VARCHAR(8),
+    name VARCHAR(128),
+    PRIMARY KEY(id)
+);
+
+CREATE TABLE IF NOT EXISTS thread_post(
+    postId VARCHAR(8),
+    threadId VARCHAR(8),
+    comment VARCHAR(4096),
+    PRIMARY KEY(postId)
+);
