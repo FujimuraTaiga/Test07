9e6217bffc339ea8bd28afdfa9a4bfcca8cbcf82
taiga-mac <fujimurataiga@icloud.com>
2022-06-11 22:57:25 +0900

    import文を整理

--- src/main/java/com/example/Test07/controller/MainPageController.java
+++ src/main/java/com/example/Test07/controller/MainPageController.java
@@ -1,7 +1,5 @@
 package com.example.Test07.controller;
 
-import com.example.Test07.repository.C8_Cafeteria.CafeteriaDAO;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.ui.Model;
