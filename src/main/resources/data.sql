718837898fe14c2aac4f6049b4f020479115c659
taiga-mac <fujimurataiga@icloud.com>
2022-06-13 20:38:11 +0900

    学食投稿データの初期値を追加

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -9,6 +9,17 @@ INSERT INTO cafeteria_menu VALUES('00000008','うどん','253円','udon');
 INSERT INTO cafeteria_menu VALUES('00000009','アジフライ','264円','flied-fish');
 INSERT INTO cafeteria_menu VALUES('00000010','フライドチキン','176円','flied-chicken');
 
+MERGE INTO cafeteria_post VALUES('aaaaaaaa','00000001',4,'うまい');
+MERGE INTO cafeteria_post VALUES('bbbbbbbb','00000002',4,'うまい');
+MERGE INTO cafeteria_post VALUES('cccccccc','00000003',4,'うまい');
+MERGE INTO cafeteria_post VALUES('dddddddd','00000004',4,'うまい');
+MERGE INTO cafeteria_post VALUES('eeeeeeee','00000005',4,'うまい');
+MERGE INTO cafeteria_post VALUES('ffffffff','00000006',4,'うまい');
+MERGE INTO cafeteria_post VALUES('gggggggg','00000007',4,'うまい');
+MERGE INTO cafeteria_post VALUES('hhhhhhhh','00000008',4,'うまい');
+MERGE INTO cafeteria_post VALUES('iiiiiiii','00000009',4,'うまい');
+MERGE INTO cafeteria_post VALUES('jjjjjjjj','00000010',4,'うまい');
+
 INSERT INTO class VALUES('00000001','工学部','情報工学科','高度情報演習IB');
 INSERT INTO class VALUES('00000002','工学部','情報工学科','データベース');
 INSERT INTO class VALUES('00000003','工学部','情報工学科','人工知能');
@@ -17,3 +28,4 @@ INSERT INTO class VALUES('00000005','工学部','機械機能工学科','熱力�
 INSERT INTO class VALUES('00000006','工学部','機械工学科','振動工学');
 INSERT INTO class VALUES('00000007','工学部','応用化学科','物理学実験');
 INSERT INTO class VALUES('00000008','システム理工学部','数理科','創る');
+
