07c077179dd525d4aac69b73a84b5dad91293128
gimashin <gima3shin@icloud.com>
2022-06-13 15:43:55 +0900

    追加します

---
+++ src/main/java/com/example/Test07/repository/C10_NoticeBoard/NoticeBoard.java
@@ -0,0 +1,5 @@
+package com.example.Test07.repository.C10_NoticeBoard;
+
+public record NoticeBoard(String id, String name) {
+
+}
---
+++ src/main/java/com/example/Test07/repository/C10_NoticeBoard/ThreadPost.java
@@ -0,0 +1,5 @@
+package com.example.Test07.repository.C10_NoticeBoard;
+
+public record ThreadPost(String postId, String threadId, String comment) {
+
+}
