1a119e2134ced9bd43784d934ea66ef893e99a67
WataruIbe <al20085@shibaura-it.ac.jp>
2022-06-13 14:33:33 +0900

    wata

---
+++ src/main/java/com/example/Test07/service/ClassService/ClassService.java
@@ -0,0 +1,40 @@
+package com.example.Test07.service.ClassService;
+
+import com.example.Test07.repository.C9_Class.Class;
+import com.example.Test07.repository.C9_Class.ClassDAO;
+import com.example.Test07.repository.C9_Class.ClassPost;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class ClassService {
+    ClassDAO dao;
+
+    @Autowired
+    ClassService(ClassDAO dao){this.dao=dao;}
+
+    public void createPost(ClassPost post){
+        dao.createPost(post);
+    }
+    public List<ClassPost> readPost(String classId){
+        return dao.readPost(classId);
+    }
+    public List<Class> readMenu(){
+        return dao.readMenu();
+    }
+    public Class findMenuById(String id){
+        return dao. findMenuById(id);
+    }
+    public String findEvaluationAVG(String classId) {
+        try {
+            double avg = dao.findEvaluationAVG(classId);
+            return String.valueOf((double) Math.round(avg*10) /10);
+        }
+    catch(NullPointerException e){
+         return "評価なし";
+
+        }
+    }
+}
