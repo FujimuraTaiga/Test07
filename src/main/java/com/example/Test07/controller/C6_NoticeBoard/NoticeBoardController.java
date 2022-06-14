81f820a5c57c506f82ff5213a6ca5132f100c1a2
gesyabadmin <al20053@shibaura-it.ac.jp>
2022-06-14 14:10:53 +0900

    八下田　変更

--- src/main/java/com/example/Test07/controller/C6_NoticeBoard/NoticeBoardController.java
+++ src/main/java/com/example/Test07/controller/C6_NoticeBoard/NoticeBoardController.java
@@ -1,4 +1,47 @@
 package com.example.Test07.controller.C6_NoticeBoard;
 
+import com.example.Test07.repository.C10_NoticeBoard.NoticeBoard;
+import com.example.Test07.repository.C10_NoticeBoard.NoticeBoardDAO;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+@RequestMapping(value = "/notice-board")
+@Controller
 public class NoticeBoardController {
+    NoticeBoardDAO dao;
+    @Autowired
+    NoticeBoardController(NoticeBoardDAO dao){ this.dao = dao; }
+
+    /*スレッド一覧表示*/
+    @RequestMapping(value = "")
+    String top(Model model){
+        model.addAttribute("noticeList",dao.readNoticeBoardList());
+        return "NoticeList.html";
+    }
+    /*スレッドの詳細の表示*/
+    @RequestMapping(value = "/detail")
+    String detail(Model model, @RequestParam String noticeId){
+        model.addAttribute("notice",dao.findNoticeBoardById(noticeId));
+        return "NoticeDetail.html";
+    }
+
+    @RequestMapping(value = "/review")
+    String review(Model model,@RequestParam String noticeId){
+        model.addAttribute("noticeId",noticeId);
+        return "Notice9999.html";
+    }
+
+    @RequestMapping(value = "/post")
+    String post(RedirectAttributes redirectAttributes,@RequestParam String noticeId){
+        dao.createPost(noticeId);
+        redirectAttributes.addAttribute("noticeId",noticeId);
+        return "redirect:/detail";
+    }
+
+
+
 }
--- src/main/java/com/example/Test07/repository/C10_NoticeBoard/NoticeBoardDAO.java
+++ src/main/java/com/example/Test07/repository/C10_NoticeBoard/NoticeBoardDAO.java
@@ -1,4 +1,28 @@
 package com.example.Test07.repository.C10_NoticeBoard;
 
-public class NoticeBoardDAO {
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
+public class NoticeBoardDAO{
+    private final JdbcTemplate jdbcTemplate;
+
+    public NoticeBoardDAO(JdbcTemplate jdbcTemplate){this.jdbcTemplate = jdbcTemplate;}
+
+    public List<NoticeBoard> readNoticeBoardList(){
+        List<NoticeBoard> threadList = new ArrayList<>();
+        threadList.add(new NoticeBoard("00000001","学食のカレーが美味すぎる件"));
+        return threadList;
+    }
+
+    public NoticeBoard findNoticeBoardById(String noticeBoardId){
+        return new NoticeBoard("00000002","学食の竜田丼が美味すぎる件");
+    }
+
+    public void createPost(String classId){
+
+    }
 }
