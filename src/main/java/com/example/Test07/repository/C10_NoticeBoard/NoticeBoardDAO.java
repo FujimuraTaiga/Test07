364ab08eb9b9d994840cad4a7114387f0d9e6958
WataruIbe <al20085@shibaura-it.ac.jp>
2022-06-21 16:31:29 +0900

    コメントの追加

--- src/main/java/com/example/Test07/repository/C10_NoticeBoard/NoticeBoardDAO.java
+++ src/main/java/com/example/Test07/repository/C10_NoticeBoard/NoticeBoardDAO.java
@@ -15,20 +15,22 @@ import java.util.Map;
 
 
 
-public class NoticeBoardDAO{
+public class NoticeBoardDAO {
     /**
      * Noticeboard
      * 掲示板のスレッドを作ることや読み取ること、スレッドに投稿することや投稿を読み取るためのクラス。
      */
     private final JdbcTemplate jdbcTemplate;
 
-    public NoticeBoardDAO(JdbcTemplate jdbcTemplate){this.jdbcTemplate = jdbcTemplate;}
+    public NoticeBoardDAO(JdbcTemplate jdbcTemplate) {
+        this.jdbcTemplate = jdbcTemplate;
+    }
 
     /**
      * 投稿情報をDB(投稿Id,スレッドId,コメント)をDB(thread_post)に登録する。
      * ＠param ThreadPost スレッドへの投稿情報を登録するためのpostId,threadId,commentを持つ。
      */
-    public void createPost(ThreadPost post){
+    public void createPost(ThreadPost post) {
         SqlParameterSource param = new BeanPropertySqlParameterSource(post);
         SimpleJdbcInsert insert = new SimpleJdbcInsert(jdbcTemplate).withTableName("thread_post");
         insert.execute(param);
@@ -39,14 +41,14 @@ public class NoticeBoardDAO{
      * 投稿情報をDB(投稿Id,スレッドId,コメント)をDB(thread_post)から読み取る。
      * ＠param threadId 投稿を識別するId
      */
-    public List<ThreadPost> readPost(String threadId){
+    public List<ThreadPost> readPost(String threadId) {
 
         String query = "SELECT * FROM thread_post WHERE threadId = ?";
 
-        List<Map<String, Object>> result = jdbcTemplate.queryForList(query,threadId);
+        List<Map<String, Object>> result = jdbcTemplate.queryForList(query, threadId);
 
         List<ThreadPost> threadPosts = result.stream()
-                .map((Map<String,Object> row) -> new ThreadPost(
+                .map((Map<String, Object> row) -> new ThreadPost(
                         row.get("postId").toString(),
                         row.get("threadId").toString(),
                         row.get("comment").toString()
@@ -58,9 +60,10 @@ public class NoticeBoardDAO{
 
     /**
      * スレッド情報（id,スレッド名）をDB(thread)に登録する。
-     * @param NoticeBoard スレッド情報を登録するためのidとnameを持つ。
+     *
+     * @param post スレッド情報を登録するためのidとnameを持つ。
      */
-    public void createThread(NoticeBoard post){
+    public void createThread(NoticeBoard post) {
 
         SqlParameterSource param = new BeanPropertySqlParameterSource(post);
         SimpleJdbcInsert insert = new SimpleJdbcInsert(jdbcTemplate).withTableName("thread");
@@ -70,19 +73,21 @@ public class NoticeBoardDAO{
 
     /**
      * スレッド情報（id,スレッド名）をDB(thread)から読み取る。
+     *
      * @return NoticeBoard スレッド情報を返す。
      */
-    public List<NoticeBoard> readThread(){
+    public List<NoticeBoard> readThread() {
         String query = "SELECT * FROM thread";
 
         List<Map<String, Object>> result = jdbcTemplate.queryForList(query);
 
-        List<NoticeBoard> NoticeBoard= result.stream()
-                .map((Map<String,Object> row) -> new NoticeBoard(
+        List<NoticeBoard> NoticeBoard = result.stream()
+                .map((Map<String, Object> row) -> new NoticeBoard(
                         row.get("id").toString(),
                         row.get("name").toString()
                 )).toList();
 
         return NoticeBoard;
     }
+}
   
