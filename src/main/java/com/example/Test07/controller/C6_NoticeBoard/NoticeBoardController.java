2b602e53b246ba4bdbc36d7544112e121e381133
gesyabadmin <al20053@shibaura-it.ac.jp>
2022-06-21 15:19:43 +0900

    NoticeBoardControllerのコメント追加

--- src/main/java/com/example/Test07/controller/C6_NoticeBoard/NoticeBoardController.java
+++ src/main/java/com/example/Test07/controller/C6_NoticeBoard/NoticeBoardController.java
@@ -13,6 +13,10 @@ import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 import java.text.AttributedString;
 import java.util.UUID;
 
+/**
+ * 掲示板に関するhttpリクエストに対応するクラス。
+ * @author YagetaSyogo
+ */
 @RequestMapping(value = "/notice")
 @Controller
 public class NoticeBoardController {
@@ -20,33 +24,46 @@ public class NoticeBoardController {
     @Autowired
     NoticeBoardController(NoticeBoardDAO dao){ this.dao = dao; }
 
-    /*スレッド一覧表示*/
+    /**
+     * NoticeDAOから授業情報のリストを取得し、htmlにそのデータを渡す。
+     * @param model
+     * @return 掲示板一覧ページを表示する。
+     */
     @RequestMapping(value = "")
     String top(Model model){
         model.addAttribute("noticeList",dao.readThread());
         return "NoticeBoardList.html";
     }
-    /*スレッドの詳細の表示*/
+
+    /**
+     * NoticeDAOからnoticeIdの一致する掲示板情報を取得し、htmlにそのデータを渡す。
+     * @param model
+     * @param noticeId 掲示板を識別するId。リクエストパラメータとして受け取る。
+     * @return 掲示板詳細ページを表示する。
+     */
     @RequestMapping(value = "/detail")
     String detail(Model model, @RequestParam String noticeId){
         model.addAttribute("noticeId",noticeId);
         model.addAttribute("postList",dao.readPost(noticeId));
         return "NoticeBoardDetail.html";
     }
-  
+
+    /**
+     * 掲示板作成にアクセスする関数
+     * @return 掲示板作成ページを表示する。
+     */
     @RequestMapping(value = "/thread")
     String review(){
         return "NoticeBoardMake.html";
     }
-    
-    @RequestMapping(value = "/make")
-    String add(@RequestParam String title){
-        String id = UUID.randomUUID().toString().substring(0,8);
-        dao.createThread(new NoticeBoard(id,title));
-        return "redirect:/notice";
-    }
+
 
     /*スレッドの新規作成*/
+    /**
+     * リクエストパラメータとしてtitleを受け取り、スレッドを新規作成する。
+     * @param title 掲示板を識別するId。
+     * @return 掲示板一覧ページへリダイレクトする。
+     */
     @RequestMapping(value = "/make")
     String make(@RequestParam String title){
         String noticeId = UUID.randomUUID().toString().substring(0,8);
@@ -54,6 +71,12 @@ public class NoticeBoardController {
         return "redirect:/notice";
     }
 
+    /**
+     * 掲示板のコメントの投稿処理を受け付ける。
+     * @param redirectAttributes リダイレクト先が要求するパラメータを付加。
+     * @param noticeId どの掲示板なのかをを識別するId。
+     * @return 掲示板詳細ページへリダイレクトする。
+     */
     @RequestMapping(value = "/post")
     String post(RedirectAttributes redirectAttributes,@RequestParam String noticeId,@RequestParam String comment){
         String postId = UUID.randomUUID().toString().substring(0,8);
